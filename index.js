c6b36e5c8cc9730608e1eaba8cc1e5f1fb7ef359
sazed1237 <sazedulislam9126@gmail.com>
2024-09-01 23:23:36 +0600

    connection off

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const enrollCollection = client.db("globalChainAcademy").collection('enrollments')
         const userCollection = client.db("globalChainAcademy").collection('users')
@@ -304,8 +304,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } catch {
         console.error("Error in run function:", error);
     }
